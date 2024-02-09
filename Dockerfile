f3d841a79901259d8f7865420f70aeba5131f570
Scott Kirkland <srkirkland@gmail.com>
2024-02-08 16:23:21 -0800

    abstraction to get the driver

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM mcr.microsoft.com/azure-functions/python:4-python3.11
+
+# Set environment variable for display port
+ENV DISPLAY=:99
+
+# Adding Google Chrome (for selenium) and Tesseract for OCR 
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+    sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list' && \
+    apt-get update && apt-get install -y \
+    google-chrome-stable \
+    tesseract-ocr \
+    libtesseract-dev \
+    poppler-utils \
+    # Add additional packages here, before the cleanup line
+    && rm -rf /var/lib/apt/lists/*  # Clean up to keep the image size down
+
+# Install Python dependencies
+COPY requirements.txt /
+RUN pip install -r /requirements.txt
+
+COPY . /home/site/wwwroot
--- download_ucop_policies.py
+++ download_ucop_policies.py
@@ -1,6 +1,8 @@
 import os
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -82,21 +84,38 @@ def get_links(driver, url):
 
     return policy_link_info_list
 
-# Set up Selenium options
-options = Options()
-options.add_argument('--headless')  # run headless Chrome
-options.add_argument('--disable-gpu')  # applicable to windows os only
-options.add_argument('--no-sandbox')  # Bypass OS security model
-options.add_argument('--disable-dev-shm-usage')  # overcome limited resource problems
+def get_driver():
+    # Try to get the chrome driver, and if not found, use our remove selenium server
+    try:
+        # Use the ChromeDriverManager to install the latest version of ChromeDriver
+        # uncomment if you are running locally and want to see the browser, then modify webdriver.Chrome to use the service
+        # service = Service(ChromeDriverManager().install())
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        driver = webdriver.Chrome(options=chrome_options)
+        return driver
+    except:
+        print("Using remote driver")
+        # Set up Selenium options
+        options = Options()
+        options.add_argument('--headless')  # run headless Chrome
+        options.add_argument('--disable-gpu')  # applicable to windows os only
+        options.add_argument('--no-sandbox')  # Bypass OS security model
+        options.add_argument('--disable-dev-shm-usage')  # overcome limited resource problems
 
-# Set up the Remote service URL pointing to where the Selenium Server is running
-remote_url = "http://selenium:4444/wd/hub"
+        # Set up the Remote service URL pointing to where the Selenium Server is running
+        remote_url = "http://selenium:4444/wd/hub"
 
-# Create a new instance of Chrome
-driver = webdriver.Remote(
-    command_executor=remote_url,
-    options=options
-)
+        # Create a new instance of Chrome
+        driver = webdriver.Remote(
+            command_executor=remote_url,
+            options=options
+        )
+        return driver
+
+driver = get_driver()
 
 # pull a list of all policies
 home_url = f'{base_url}/advanced-search.php?action=welcome&op=browse&all=1'
