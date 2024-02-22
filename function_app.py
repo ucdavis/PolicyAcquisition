63b705bb2692901d12a64570eb806433595fe02b
Scott Kirkland <srkirkland@gmail.com>
2024-02-21 16:30:54 -0800

    only run once a day at midnight

--- function_app.py
+++ function_app.py
@@ -11,11 +11,11 @@ app = func.FunctionApp()
 def utc_timestamp() -> str:
     return datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc).isoformat()
 
-@app.timer_trigger(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True,
+@app.timer_trigger(schedule="0 0 8 * * 1-5", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def ProcessUcop(myTimer: func.TimerRequest) -> None:
     print('Python timer trigger function ran at %s', utc_timestamp())
-    
+
     logging.info('Starting Ucop Download %s', utc_timestamp())
     
     # Download the UCOP policies
