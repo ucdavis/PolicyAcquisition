71566f4982f626dc8112655a77c2f81345183f46
Scott Kirkland <srkirkland@gmail.com>
2024-03-26 09:55:11 -0700

    add some parent metadata and index fuill text too

--- vectorize.py
+++ vectorize.py
@@ -28,10 +28,16 @@ if not API_KEY:
 ELASTIC_URL = os.getenv("ELASTIC_URL", "http://127.0.0.1:9200")
 ELASTIC_WRITE_USERNAME = os.getenv("ELASTIC_WRITE_USERNAME", "")
 ELASTIC_WRITE_PASSWORD = os.getenv("ELASTIC_WRITE_PASSWORD", "")
-ELASTIC_INDEX = os.getenv("ELASTIC_INDEX", "policy_vectorstore_dev")
+ELASTIC_INDEX = os.getenv("ELASTIC_INDEX", "policy_vectorstore_test")
+ELASTIC_INDEX_FULLTEXT = os.getenv("ELASTIC_INDEX_FULLTEXT", "policy_fulltext_test")
 
 # Create our elastic client
-es_client = Elasticsearch(hosts=[ELASTIC_URL], basic_auth=(ELASTIC_WRITE_USERNAME, ELASTIC_WRITE_PASSWORD), max_retries=10, retry_on_timeout=True)
+es_client = Elasticsearch(
+    hosts=[ELASTIC_URL],
+    basic_auth=(ELASTIC_WRITE_USERNAME, ELASTIC_WRITE_PASSWORD),
+    max_retries=10,
+    retry_on_timeout=True,
+)
 
 # might want to play with `text-embedding-3-small` later
 embedding = OpenAIEmbeddings(model="text-embedding-3-large")
@@ -86,8 +92,11 @@ def load_documents_from_folder(folder_path, update_progress):
             with open(content_path, "r") as file:
                 data["content"] = file.read()
                 text = data["content"].encode("utf-8")
+                # create hash
                 md5_hash.update(text)
                 data["hash"] = md5_hash.hexdigest()
+                # include length of content in full document
+                data["content_length"] = len(data["content"])
         else:
             pass
             logger.error(f"Content file {content_path} not found")
@@ -99,12 +108,14 @@ def load_documents_from_folder(folder_path, update_progress):
 def process_folders(folders, update_progress):
     """
     Processes the folders and their contents, then pushes the vectorized data to Elasticsearch.
+    Also push the full text to a separate index
 
     Args:
         folders: A dictionary of folder names and paths.
         update_progress: A callback function to update the progress of the vectorization.
     """
     real_index_name = "policy_vectorstore_" + str(int(time.time()))
+    real_full_text_index_name = "policy_fulltext_" + str(int(time.time()))
 
     for folder_name, folder_path in folders.items():
         update_progress(f"Processing {folder_name}")
@@ -112,11 +123,9 @@ def process_folders(folders, update_progress):
 
         documents_to_index = []
         for document in documents:
-            ## TODO:
             ## Filter out ignored classifications
             ## Turn into documents for langchain to split
             ## vectorize and push into ES, probably in batches (200 at a time seems ok)
-            # print(document)
 
             # Make sure document has content
             if "content" in document:
@@ -148,7 +157,7 @@ def process_folders(folders, update_progress):
         # now we have a bunch of documents to index for this folder
 
         # split - TODO: play with chunk size and overlap
-        text_splitter = RecursiveCharacterTextSplitter()
+        text_splitter = RecursiveCharacterTextSplitter(add_start_index=True)
 
         splitDocs = text_splitter.split_documents(documents_to_index)
 
@@ -169,16 +178,31 @@ def process_folders(folders, update_progress):
                 es_connection=es_client,
             )
 
-        update_progress(f"Finished processing {folder_name}")
-    
-    update_progress(f"Finished processing all folders. New index is {real_index_name}. Swapping aliases...")
+        # push full text to separate index
+        for i in range(0, len(documents_to_index), chunk_size):
+            update_progress(
+                f"Processing full text {i} to {i+chunk_size} of {len(documents_to_index)} in {folder_name}"
+            )
+            ElasticsearchStore.from_documents(
+                documents_to_index[i : i + chunk_size],
+                embedding=None,  # don't create embeddings for full text
+                index_name=real_full_text_index_name,
+                es_connection=es_client,
+            )
 
-    create_and_update_alias(real_index_name)
+        update_progress(f"Finished processing {folder_name}")
+
+    update_progress(
+        f"Finished processing all folders. New index is {real_index_name}. Swapping aliases..."
+    )
+
+    create_and_update_alias(real_index_name, ELASTIC_INDEX)
+    create_and_update_alias(real_full_text_index_name, ELASTIC_INDEX_FULLTEXT)
 
     update_progress(f"Alias updated to {real_index_name}")
 
 
-def create_and_update_alias(index_name):
+def create_and_update_alias(index_name, alias_name):
     """
     We are going to swap the alias from the current index to the new index given by `index_name`
 
@@ -191,7 +215,7 @@ def create_and_update_alias(index_name):
     # Create our elastic client
 
     # Find all indexes the alias points to currently
-    alias_exists = es_client.indices.exists_alias(name=ELASTIC_INDEX)
+    alias_exists = es_client.indices.exists_alias(name=alias_name)
 
     # setup actions to perform
     actions = []
@@ -200,7 +224,7 @@ def create_and_update_alias(index_name):
         {
             "add": {
                 "index": index_name,
-                "alias": ELASTIC_INDEX,
+                "alias": alias_name,
             }
         }
     )
@@ -209,7 +233,7 @@ def create_and_update_alias(index_name):
 
     if alias_exists:
         # Get all indexes the alias points to
-        indexes = es_client.indices.get_alias(name=ELASTIC_INDEX)
+        indexes = es_client.indices.get_alias(name=alias_name)
 
         for index in indexes:
             if index != index_name:
@@ -218,11 +242,11 @@ def create_and_update_alias(index_name):
                 {
                     "remove": {
                         "index": index,
-                        "alias": ELASTIC_INDEX,
+                        "alias": alias_name,
                     }
                 }
             )
-    
+
     # Execute -- update the alias (remove from old indices and add to new index)
     es_client.indices.update_aliases(body={"actions": actions})
 
