24d7b0c38c9c8c16a61bde1701361a4e5704e634
Scott Kirkland <srkirkland@gmail.com>
2024-06-25 15:22:45 -0700

    better logging for our exceptions

--- watchdog.py
+++ watchdog.py
@@ -1,16 +1,20 @@
 import subprocess
 import time
 
+from background.logger import setup_logger
+
+logger = setup_logger()
+
 command = ["python", "background/update.py"]
 
 # Just run the update script in a loop, restarting if it fails
 
 while True:
     try:
-        print(f"Starting process: {' '.join(command)}")
+        logger.info(f"Starting process: {' '.join(command)}")
         process = subprocess.Popen(command)
         process.wait()
     except Exception as e:
-        print(f"Process failed with error: {e}")
-    print("Restarting process in 5 seconds...")
+        logger.exception(f"Process failed with error: {e}")
+    logger.info("Restarting process in 5 seconds...")
     time.sleep(5)
